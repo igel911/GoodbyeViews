5098001dced7cc8998ff46e9b882e2b2641990c4
Khmelnytskiy <igel4@ukr.net>
2023-07-20 09:08:15 +0300

    Added the focus changer to code inputs

--- app/src/main/java/com/example/goodbyeviews/ui/views/SmsCodeInput.kt
+++ app/src/main/java/com/example/goodbyeviews/ui/views/SmsCodeInput.kt
@@ -5,14 +5,22 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.requiredWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
@@ -21,12 +29,17 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.goodbyeviews.ui.theme.GoodbyeViewsTheme
 
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
 @Composable
 fun SmsCodeInput() {
     var valueFirst by remember { mutableStateOf("") }
     var valueSecond by remember { mutableStateOf("") }
     var valueThird by remember { mutableStateOf("") }
     var valueFourth by remember { mutableStateOf("") }
+    val (secondFocus, thirdFocus, fourthFocus) = remember {
+        FocusRequester.createRefs()
+    }
+
     Box(modifier = Modifier.fillMaxWidth()) {
         Row(
             modifier = Modifier.align(Alignment.Center),
@@ -36,37 +49,75 @@ fun SmsCodeInput() {
                 keyboardType = KeyboardType.Number,
                 imeAction = ImeAction.Next
             )
-            OutlinedTextInput(
+            val colors = TextFieldDefaults.outlinedTextFieldColors(
+                focusedBorderColor = Color.Blue,
+                unfocusedBorderColor = Color.Gray
+            )
+
+            OutlinedTextField(
                 modifier = Modifier.requiredWidth(48.dp),
+                singleLine = true,
                 value = valueFirst,
                 onValueChange = { value ->
                     valueFirst = value
                     if (value.length == 1) {
-                        //onNextAction()
+                        secondFocus.requestFocus()
                     }
                 },
+                shape = RoundedCornerShape(10.dp),
                 keyboardOptions = keyboardOptions,
-                textStyle = TextStyle(textAlign = TextAlign.Center)
+                textStyle = TextStyle(textAlign = TextAlign.Center),
+                colors = colors
             )
 
-            OutlinedTextInput(
-                modifier = Modifier.requiredWidth(48.dp),
+            OutlinedTextField(
+                modifier = Modifier
+                    .requiredWidth(48.dp)
+                    .focusRequester(secondFocus),
+                singleLine = true,
                 value = valueSecond,
-                onValueChange = { value -> valueSecond = value },
-                keyboardOptions = keyboardOptions
+                onValueChange = { value ->
+                    valueSecond = value
+                    if (value.length == 1) {
+                        thirdFocus.requestFocus()
+                    }
+                },
+                shape = RoundedCornerShape(10.dp),
+                keyboardOptions = keyboardOptions,
+                textStyle = TextStyle(textAlign = TextAlign.Center),
+                colors = colors
             )
 
-            OutlinedTextInput(
-                modifier = Modifier.requiredWidth(48.dp),
+            OutlinedTextField(
+                modifier = Modifier
+                    .requiredWidth(48.dp)
+                    .focusRequester(thirdFocus),
+                singleLine = true,
                 value = valueThird,
-                onValueChange = { value -> valueThird = value },
-                keyboardOptions = keyboardOptions
+                onValueChange = { value ->
+                    valueThird = value
+                    if (value.length == 1) {
+                        fourthFocus.requestFocus()
+                    }
+                },
+                shape = RoundedCornerShape(10.dp),
+                keyboardOptions = keyboardOptions,
+                textStyle = TextStyle(textAlign = TextAlign.Center),
+                colors = colors
             )
 
-            OutlinedTextInput(
-                modifier = Modifier.requiredWidth(48.dp),
+            OutlinedTextField(
+                modifier = Modifier
+                    .requiredWidth(48.dp)
+                    .focusRequester(fourthFocus),
+                singleLine = true,
                 value = valueFourth,
-                onValueChange = { value -> valueFourth = value },
+                onValueChange = { value ->
+                    valueFourth = value
+                },
+                shape = RoundedCornerShape(10.dp),
+                textStyle = TextStyle(textAlign = TextAlign.Center),
+                colors = colors,
                 keyboardOptions = keyboardOptions.copy(imeAction = ImeAction.Done)
             )
         }
