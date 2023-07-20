65cc6b8e3db87c240078fee5f569fc9d1dbc66b6
Khmelnytskiy <igel4@ukr.net>
2023-07-20 11:04:27 +0300

    Added a picture to the login screen

--- app/src/main/java/com/example/goodbyeviews/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/example/goodbyeviews/ui/screens/LoginScreen.kt
@@ -1,5 +1,6 @@
 package com.example.goodbyeviews.ui.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -7,13 +8,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.goodbyeviews.R
 import com.example.goodbyeviews.ui.theme.GoodbyeViewsTheme
 import com.example.goodbyeviews.ui.views.LoginOptions
 
@@ -31,10 +32,11 @@ fun LoginScreen(
                 .fillMaxWidth()
                 .height(300.dp)
         ) {
-            Text(
-                modifier = Modifier.align(Alignment.Center),
-                textAlign = TextAlign.Center,
-                text = "Here should be a picture but I messed up :("
+            Image(
+                painterResource(R.drawable.real_work),
+                contentDescription = "",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.fillMaxSize()
             )
         }
         LoginOptions(navigateForward)
